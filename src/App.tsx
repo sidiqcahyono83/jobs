c384d51bfb1f1c96e9dde3fe94b6b8e5f207ec5d
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-22 07:21:02 +0700

    fate: container

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,31 @@ import { dataJobs } from "./data/datajob";
 export function App() {
   return (
     <>
-      <div className="m-10 flex justify-center max-w-5xl">
-        <h1 className="font-bold text-4xl text-slate-800 font-neutral-400">
-          MY JOBS
-        </h1>
-      </div>
+      <main className="container mx-auto">
+        <div className="m-10 flex justify-center max-w-5xl">
+          <h1 className="font-bold text-4xl text-slate-800 font-neutral-400">
+            MY JOBS
+          </h1>
+        </div>
 
-      <div className="mx-10 max-w-5xl">
-        <h2 className="font-bold text-2xl text-slate-800 font-neutral-400">
-          Job Desk
-        </h2>
-        <hr />
-        <section className="my-4">
-          <div>
-            <ul className="flex-row text-2xl  justify-between gap-2">
-              {dataJobs.map((Jobs) => (
-                <li key={Jobs.id} className="my-2">
-                  <JobItem job={Jobs} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </section>
-      </div>
+        <div className="mx-10 max-w-5xl">
+          <h2 className="font-bold text-2xl text-slate-800 font-neutral-400">
+            Job Desk
+          </h2>
+          <hr />
+          <section className="my-4">
+            <div>
+              <ul className="flex-row text-2xl  justify-between gap-2">
+                {dataJobs.map((Jobs) => (
+                  <li key={Jobs.id} className="my-2">
+                    <JobItem job={Jobs} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+        </div>
+      </main>
     </>
   );
 }
