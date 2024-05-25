04b52d72fa7eb1e1b73c2268e2048d94afb6be9d
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-25 22:44:13 +0700

    fate: outside module

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,19 @@
 export function HomeRoute() {
   return (
-    <div className="flex flex-col  my-4 mx-4 text-2xl max-xl:">
+    <div className="flex my-4 mx-4 text-2xl max-w-96">
       <h1 className="text-blue-800 font-bold ">This Route Home</h1>
-      <ul className="flex flex-col my-6">
-        <li>Home</li>
-        <li>Dasboard</li>
-        <li>Jobs Item</li>
-        <li>Jobs List</li>
-      </ul>
+      <div className="flex">
+        <ul className="flex flex-col my-6">
+          <li>Home</li>
+          <li>Dasboard</li>
+          <li>Jobs Item</li>
+          <li>Jobs List</li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>Content</h1>
+      </div>
     </div>
   );
 }
