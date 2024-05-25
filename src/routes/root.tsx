d2a0cf77c2cfea31628c09afe9f398251b6cdc60
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-25 08:53:11 +0700

    fate: react router dom

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,35 +1,23 @@
 import { useState } from "react";
-import { JobItem } from "./components/ui/job-item";
-import { JobType, dataJobs } from "./data/jobs";
+import { JobType, dataJobs } from "../data/jobs";
+import { JobItem } from "../components/ui/job-item";
 
-
-export function App() {
-
+export function Root() {
   const [jobsState, setJobs] = useState(dataJobs);
-
   const saveNewJob = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    
-
     const formData = new FormData(event.currentTarget);
-  
-
     const newdataJob: JobType = {
       id: 4,
       title: formData.get("title")?.toString() || "Untitled",
       category: formData.get("category")?.toString() || "Uncategorized",
-      divisi: formData.get("divisi")?.toString() || "Tecknik" ,
-      isDone:  false ,
+      divisi: formData.get("divisi")?.toString() || "Tecknik",
+      isDone: false,
       timeStart: new Date("2024-05-22 08:30"),
-      timeEnd: new Date("2024-05-22 14:30")
+      timeEnd: new Date("2024-05-22 14:30"),
     };
-
     setJobs([...jobsState, newdataJob]);
   };
-
-
-
   return (
     <>
       <main className="container mx-auto dark:text-white">
@@ -45,7 +33,6 @@ export function App() {
           </h2>
           <hr />
           <div className="my-2 dark:text-white">
-            
             <form action="" onSubmit={saveNewJob} className="mx-auto">
               <div className="form-control my-2">
                 <label htmlFor="title" className="font-normal text-2xl my-4">
@@ -86,20 +73,19 @@ export function App() {
                   required
                 />
               </div>
-             
-                <button
-                  type="submit"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 mx-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Save
-                </button>
-                <button
-                  type="reset"
-                  className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-                >
-                  Reset
-                </button>
-              
+
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 mx-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Save
+              </button>
+              <button
+                type="reset"
+                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+              >
+                Reset
+              </button>
             </form>
           </div>
 
@@ -109,7 +95,7 @@ export function App() {
             <div>
               <ul className="flex-row text-2xl  justify-between gap-2">
                 {jobsState.map((Jobs) => (
-                  <li key={Jobs.id} className="my-2 ">
+                  <li key="{Jobs.id}" className="my-2 ">
                     <JobItem job={Jobs} />
                   </li>
                 ))}
