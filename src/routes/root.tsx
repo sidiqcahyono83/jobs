b8969c950425b4f89eb3ca02d7d30994c4e8948a
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-26 00:53:37 +0700

    fate: add route

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,8 @@
 import { FaStackOverflow } from "react-icons/fa";
-import { FaAlignLeft } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
+
 import { Link, Outlet } from "react-router-dom";
+
 export function RootRoute() {
   return (
     <main>
@@ -9,18 +10,12 @@ export function RootRoute() {
         <h1 className="text-slate-950 m-10 dark:text-slate-100 font-bold justify-center text-6xl flex">
           <Link to="/">MY JOBS</Link>
         </h1>
-        <div className="flex flex-col-2">
-          <ul className="flex flex-col gap-2  text-2xl">
+        <div className="flex  justify-evenly">
+          <ul className="flex flex-col gap-2 text-2xl">
             <li className=" gap-2  hover:text-blue-800">
               <Link to="/jobs">
                 <FaStackOverflow />
                 My Jobs
-              </Link>
-            </li>
-            <li className="flex gap-2 items-center hover:text-blue-800">
-              <Link to="">
-                <FaAlignLeft />
-                Jobs Item
               </Link>
             </li>
             <li className="flex gap-2 items-center  hover:text-blue-800">
