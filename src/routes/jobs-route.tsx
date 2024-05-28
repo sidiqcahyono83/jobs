9dbdfc0bca4a48addd836306db5b2441bd675b7b
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-28 21:21:11 +0700

    fate: fix

--- src/routes/jobs-route.tsx
+++ src/routes/jobs-route.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
 import { JobItem } from "../components/ui/job-item";
 import { Link } from "react-router-dom";
-import localforage from "localforage";
+
 import { dataJobs } from "../data/jobs";
 
 export function JobsRoute() {
   return (
     <>
       <div>
-        <h1 className="font-bold mx-4 text-2xl rounded-sm text-slate-800 font-neutral-400 flex-row dark:text-white border-2">
+        <h1 className="font-bold mx-4 text-2xl  text-slate-800 font-neutral-400 flex-row dark:text-white items-stretch">
           Jobs List
         </h1>
         <ul>
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,15 +2,17 @@ import { FaStackOverflow } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 
 import { Link, Outlet } from "react-router-dom";
+import { HomeRoute } from "./home";
 
 export function RootRoute() {
   return (
-    <main className="box-border justify-items-center max-w-screen-xl content-center mx-auto border-2">
-      <h1 className="text-slate-950 hover:text-teal-800 m-10 dark:text-slate-100 font-bold justify-center text-4xl flex">
-        <Link to="/">MY JOBS</Link>
-      </h1>
-      <div className="flex max-w-full">
-        <ul className="flex w-3/12 max-h-full items-center gap-2 text-xl font-bold flex-col border-2">
+    <main className="box-border justify-items-center max-w-screen-xl content-center mx-auto">
+      <div className="flex flex-row justify-between items-center">
+        <h1 className="text-slate-950 hover:text-teal-800 m-10 dark:text-slate-100 font-bold justify-center text-4xl">
+          <Link to="/">MY JOBS</Link>
+        </h1>
+
+        <ul className="w-3/12 max-h-full items-center gap-2 text-xl font-bold flex flex-row items-center">
           <li className="hover:text-blue-800">
             <Link to="/jobs" className="flex gap-2">
               <FaStackOverflow />
@@ -24,11 +26,9 @@ export function RootRoute() {
             </Link>
           </li>
         </ul>
-
-        <section>
-          <Outlet />
-        </section>
       </div>
+
+      <Outlet />
     </main>
   );
 }
