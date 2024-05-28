4d7618b98075727b0aa235b4b0f51a237beb04c0
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-28 20:47:20 +0700

    fate: route

--- src/routes/jobs-route.tsx
+++ src/routes/jobs-route.tsx
@@ -5,30 +5,25 @@ import localforage from "localforage";
 import { dataJobs } from "../data/jobs";
 
 export function JobsRoute() {
-  
-
   return (
     <>
-        <div>
-            <h1 className="font-bold mx-4 text-4xl rounded-sm text-slate-800 font-neutral-400 dark:text-white">
-                    Jobs List
-                </h1>
-                <ul>
-                    {dataJobs.map((job) => (
-                        <li
-                            key={job.id}
-                            className="my-2 dark:text-white hover:rounded-xl"
-                        >
-                            <Link
-                                to={`/jobs/${job.id}`}
-                                className="block hover:bg-stone-700"
-                            >
-                                <JobItem job={job}/>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-        </div>
+      <div>
+        <h1 className="font-bold mx-4 text-2xl rounded-sm text-slate-800 font-neutral-400 flex-row dark:text-white border-2">
+          Jobs List
+        </h1>
+        <ul>
+          {dataJobs.map((job) => (
+            <li
+              key={job.id}
+              className="dark:text-white hover:rounded-xl text-lg mx-2"
+            >
+              <Link to={`/jobs/${job.id}`} className="block hover:bg-stone-700">
+                <JobItem job={job} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
