34d74f541f72d83add79d2f625932e8565b76eb1
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-31 23:18:19 +0700

    fix add

--- src/routes/add-job.tsx
+++ src/routes/add-job.tsx
@@ -4,105 +4,122 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export function AddJobRoute() {
-  const [jobsState, setJobs] = useState<Job[]>([]);
+	const [jobs, setJobs] = useState<Job[]>([]);
 
-  const saveNewJob = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const newJob: Job = {
-      id: length + 1,
-      title: formData.get("title")?.toString() || "Untitled",
-      category: formData.get("category")?.toString() || "Uncategorized",
-      divisi: formData.get("divisi")?.toString() || "Teknik",
-      isDone: false,
-      timeStart: new Date("2024-05-22 08:30"),
-      timeEnd: new Date("2024-05-22 14:30")
-    };
+	const saveNewJob = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const newJob: Job = {
+			id: length + 1,
+			title: formData.get("title")?.toString() || "Untitled",
+			category: formData.get("category")?.toString() || "Uncategorized",
+			divisi: formData.get("divisi")?.toString() || "Teknik",
+			isDone: false,
+			timeStart: new Date("2024-05-22 08:30"),
+			timeEnd: new Date("2024-05-22 14:30")
+		};
 
-    const updatedJobs = [...jobsState, newJob];
-    setJobs(updatedJobs);
-    localforage.setItem("jobs", updatedJobs);
-  };
+		const updatedJobs = [...jobs, newJob];
+		setJobs(updatedJobs);
+		console.log(updatedJobs);
 
-  return (
-    <div>
-      <div className="my-2 dark:text-white">
-        <h1 className="text-4xl font-bold my-4 mb-4">Add Job</h1>
-        <form onSubmit={saveNewJob} className="mx-auto">
-          <div className="form-control my-2 items-center">
-            <label htmlFor="title" className="font-normal text-2xl my-4">
-              Title Job :
-            </label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              required
-            />
-          </div>
-          <div className="form-control my-2">
-            <label htmlFor="category" className="font-normal text-2xl my-4">
-              Category Job :
-            </label>
-            <input
-              type="text"
-              name="category"
-              id="category"
-              className="mx-10 rounded-md  text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              required
-            />
-          </div>
-          <div className="form-control my-2">
-            <label htmlFor="divisi" className="font-normal text-2xl my-4">
-              Divisi :
-            </label>
-            <input
-              type="text"
-              name="divisi"
-              id="divisi"
-              className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              required
-            />
-          </div>
-          <div className="form-control my-2">
-            <label htmlFor="timeStart" className="font-normal text-2xl my-4">
-              Time Start :
-            </label>
-            <input
-              type="date"
-              name="timeStart"
-              id="timeStart"
-              className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              required
-            />
-          </div>
-          <div className="form-control my-2">
-            <label htmlFor="timeEnd" className="font-normal text-2xl my-4">
-              Time Start :
-            </label>
-            <input
-              type="date"
-              name="timeEnd"
-              id="timeEnd"
-              className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-4 py-2 mx-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            <Link to="/jobs">Save</Link>
-          </button>
-          <button
-            type="reset"
-            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg w-full sm:w-auto px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-          >
-            Reset
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+		localforage.setItem("jobs", updatedJobs);
+	};
+
+	return (
+		<div>
+			<div className="my-2 dark:text-white">
+				<h1 className="text-4xl font-bold my-4 mb-4">Add Job</h1>
+				<form onSubmit={saveNewJob} className="mx-auto">
+					<div className="form-control my-2 items-center">
+						<label
+							htmlFor="title"
+							className="font-normal text-2xl my-4"
+						>
+							Title Job :
+						</label>
+						<input
+							type="text"
+							name="title"
+							id="title"
+							className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+							required
+						/>
+					</div>
+					<div className="form-control my-2">
+						<label
+							htmlFor="category"
+							className="font-normal text-2xl my-4"
+						>
+							Category Job :
+						</label>
+						<input
+							type="text"
+							name="category"
+							id="category"
+							className="mx-10 rounded-md  text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+							required
+						/>
+					</div>
+					<div className="form-control my-2">
+						<label
+							htmlFor="divisi"
+							className="font-normal text-2xl my-4"
+						>
+							Divisi :
+						</label>
+						<input
+							type="text"
+							name="divisi"
+							id="divisi"
+							className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+							required
+						/>
+					</div>
+					<div className="form-control my-2">
+						<label
+							htmlFor="timeStart"
+							className="font-normal text-2xl my-4"
+						>
+							Time Start :
+						</label>
+						<input
+							type="date"
+							name="timeStart"
+							id="timeStart"
+							className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+							required
+						/>
+					</div>
+					<div className="form-control my-2">
+						<label
+							htmlFor="timeEnd"
+							className="font-normal text-2xl my-4"
+						>
+							Time End :
+						</label>
+						<input
+							type="date"
+							name="timeEnd"
+							id="timeEnd"
+							className="mx-10 rounded-md text-2xl text-gray-900 bg-transparent border-2 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+							required
+						/>
+					</div>
+					<button
+						type="submit"
+						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-4 py-2 mx-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					>
+						<Link to="/jobs">Save</Link>
+					</button>
+					<button
+						type="reset"
+						className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-lg w-full sm:w-auto px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+					>
+						Reset
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
--- src/routes/job.tsx
+++ src/routes/job.tsx
@@ -2,29 +2,31 @@ import { useParams } from "react-router-dom";
 import { dataJobs } from "../data/jobs";
 
 export function DetailJobRoute() {
-  const id = useParams();
-  const jobId = Number(id);
+	const idParam = useParams();
+	const jobId = Number(idParam.jobId);
 
-  const job = dataJobs.find((job) => job.id == jobId);
+	const job = dataJobs.find((job) => job.id === jobId);
 
-  if (!job) {
-    return <p>Job not found.</p>;
-  }
+	if (!job) {
+		return <p>Job not found.</p>;
+	}
 
-  return (
-    <div className="my-4 mx-10">
-      <h1 className="text-4xl font-bold mb-4">{job.title}</h1>
-      <div className="text-xl">
-        <p>Category: {job.category}</p>
-        <p>Divisi: {job.divisi}</p>
-        <p>Is Done: {job.isDone ? "Yes" : "No"}</p>
-        {job.timeStart && (
-          <p>Time Start: {new Date(job.timeStart).toLocaleString()}</p>
-        )}
-        {job.timeEnd && (
-          <p>Time End: {new Date(job.timeEnd).toLocaleString()}</p>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="my-4 mx-10">
+			<h1 className="text-4xl font-bold mb-4">{job.title}</h1>
+			<div className="text-xl">
+				<p>Category: {job.category}</p>
+				<p>Divisi: {job.divisi}</p>
+				<p>Is Done: {job.isDone ? "Yes" : "No"}</p>
+				{job.timeStart && (
+					<p>
+						Time Start: {new Date(job.timeStart).toLocaleString()}
+					</p>
+				)}
+				{job.timeEnd && (
+					<p>Time End: {new Date(job.timeEnd).toLocaleString()}</p>
+				)}
+			</div>
+		</div>
+	);
 }
